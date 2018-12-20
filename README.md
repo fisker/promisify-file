e51693b2cfa5dee8f6dfbb371a0081942b531d15
fisker <lionkay@gmail.com>
2018-12-20 11:35:26 +0800

    new api doc

--- README.md
+++ README.md
@@ -6,21 +6,167 @@
 
 takes a `Blob` as argument
 
-### PromisifyFile.prototype.arrayBuffer
+### methods
 
-return Promise&lt;ArrayBuffer&gt; use `FileReader.readAsArrayBuffer`
+#### PromisifyFile.prototype.{arrayBuffer, text, dataURL, binaryString}
 
-### PromisifyFile.prototype.text
+returns a `Promise` that resolves with the result `FileReader.readAs{ArrayBuffer, Text, DataURL, BinaryString}`
 
-return Promise&lt;String&gt; use `FileReader.readAsText`
+syntax
 
-### PromisifyFile.prototype.dataURL
+```js
+file.arrayBuffer()
 
-return Promise&lt;String&gt; use `FileReader.readAsDataURL`
+// optional `encoding` use in `FileReader.readAsText`
+file.text([encoding])
 
-### PromisifyFile.prototype.binaryString
+file.dataURL()
 
-return Promise&lt;String&gt; use `FileReader.readAsBinaryString`
+file.binaryString()
+```
+
+* result `arrayBuffer / text(UTF-8) / dataURL / binaryString` is cached
+
+[FileReader](https://developer.mozilla.org/en-US/docs/Web/API/FileReader/FileReader)
+
+#### PromisifyFile.prototype.{int8Array, uint8Array, uint8ClampedArray, int16Array, uint16Array, int32Array, uint32Array, float32Array, float64Array}
+
+returns a `Promise` that resolves with a `TypedArray` created with blob arrayBuffer
+
+optional arguments `byteOffset` and `length` same as `TypedArray` syntax
+
+syntax
+
+```js
+file.int8Array([byteOffset [, length]])
+
+file.uint8Array([byteOffset [, length]])
+
+file.uint8ClampedArray([byteOffset [, length]])
+
+file.int16Array([byteOffset [, length]])
+
+file.uint16Array([byteOffset [, length]])
+
+file.uint32Array([byteOffset [, length]])
+
+file.float32Array([byteOffset [, length]])
+
+file.float64Array([byteOffset [, length]])
+```
+
+[TypedArray](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/TypedArray)
+
+#### PromisifyFile.prototype.dataView
+
+returns a `Promise` that resolves with a `DataView` created with blob arrayBuffer
+
+optional arguments `byteOffset` and `length` same as `DataView` syntax
+
+syntax
+
+```js
+file.dataView([byteOffset [, length]])
+```
+
+[DataView](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/DataView)
+
+#### PromisifyFile.prototype.blob
+
+returns a `Promise` that resolves with a new `Blob` created with orignal blob
+
+optional argument `options` same as `Blob` syntax
+
+syntax
+
+```js
+file.blob([options]])
+```
+
+[Blob](https://developer.mozilla.org/en-US/docs/Web/API/Blob/Blob)
+
+#### PromisifyFile.prototype.file
+
+returns a `Promise` that resolves with a new `File` created with orignal blob
+
+optional arguments `name` and `options` same as `File` syntax
+
+syntax
+
+```js
+file.file([name [, options]])
+```
+
+* if name is omitted, and the orignal `Blob` don't has a name. a `TypeError` throws *
+
+[File](https://developer.mozilla.org/en-US/docs/Web/API/File/File)
+
+#### PromisifyFile.prototype.json
+
+returns a `Promise` that resolves with the result of parsing blob text as JSON
+
+syntax
+
+```js
+file.json([encoding [, reviver]])
+```
+
+[JSON.parse](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse)
+
+#### PromisifyFile.prototype.url
+
+returns a `Promise` that resolves with the result of `URL.createObjectURL` create with blob
+
+syntax
+
+```js
+file.url()
+```
+
+[URL.createObjectURL](https://developer.mozilla.org/en-US/docs/Web/API/URL/createObjectURL)
+
+#### PromisifyFile.prototype.image
+
+returns a `Promise` that resolves with loaded `HTMLImageElement` create with blob
+
+syntax
+
+```js
+file.image()
+```
+
+[HTMLImageElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLImageElement)
+
+#### PromisifyFile.prototype.imageBitmap
+
+returns a `Promise` that resolves with `ImageBitmap` that `createImageBitmap` returns
+
+syntax
+
+```js
+file.imageBitmap([options])
+
+file.imageBitmap(sx, sy, sw, sh[, options])
+```
+
+[createImageBitmap](https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/createImageBitmap)
+[ImageBitmap](https://developer.mozilla.org/en-US/docs/Web/API/ImageBitmap)
+
+### PromisifyFile.prototype.imageData
+
+returns a `Promise` that resolves with `ImageData` containing the image data of orignal image blob
+
+syntax
+
+```js
+file.imageData([sx = 0[, sy = 0[, sw = image.width [, sh = image.height]]]])
+```
+
+
+[ImageData](https://developer.mozilla.org/en-US/docs/Web/API/ImageData/ImageData)
+[HTMLCanvasElement](https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement)
+[CanvasRenderingContext2D](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D)
+[CanvasRenderingContext2D.getImageData](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/getImageData)
 
 ## Usage
 
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 
   var root = Function('return this')() // eslint-disable-line
 
+  var document = root.document
+
   var FileReader = root.FileReader
   var Promise = root.Promise
   var Blob = root.Blob
@@ -16,7 +18,7 @@
 
   var concat = Array.prototype.concat
 
-  var dataTypes = [
+  var fileReaderSupportedDataTypes = [
     'ArrayBuffer',
     'Text',
     'DataURL',
@@ -36,6 +38,33 @@
     'DataView'
   ]
 
+  /**
+   * Faster apply
+   * Call is faster than apply, optimize less than 4 args *
+   *
+   * @param  {Function} func
+   * @param  {any} context
+   * @param  {Array} args
+   */
+  function apply (func, context, args) {
+    switch (args.length) {
+      case 0:
+        return func.call(context)
+      case 1:
+        return func.call(context, args[0])
+      case 2:
+        return func.call(context, args[0], args[1])
+      case 3:
+        return func.call(context, args[0], args[1], args[2])
+      default:
+        return func.apply(context, args)
+    }
+  }
+
+  function camelcase (s) {
+    return s[0].toLowerCase() + s.slice(1)
+  }
+
   function umdExport (name, mod) {
     if (typeof define === 'function' && define.amd) {
       define(name, [], function () {
@@ -50,7 +79,20 @@
     return mod
   }
 
-  function readAsPromise (method, file, args) {
+  function prepend (first, rest) {
+    return apply(concat, [first], rest)
+  }
+
+  function PromisifyFile (blob, options) {
+    this.$store = {
+      orignal: blob
+    }
+    this.$options = options
+  }
+
+  // use FileReader `readAs...` method
+
+  function readFile (method, blob, args) {
     var fileReader = new FileReader()
 
     return new Promise(function (resolve, reject) {
@@ -62,37 +104,39 @@
         reject(fileReader.error)
       }
 
-      apply(method, fileReader, prepend(file, args))
+      apply(method, fileReader, prepend(blob, args || []))
     })
   }
 
-  function apply (method, context, args) {
-    switch (args.length) {
-      case 0:
-        return method.call(context)
-      case 1:
-        return method.call(context, args[0])
-      case 2:
-        return method.call(context, args[0], args[1])
-      default:
-        return method.apply(context, args)
-    }
-  }
-
-  function callFileReaderMethod (methodName) {
-    var method = FileReader.prototype[methodName]
+  function readFileAs (dataType) {
+    var method = FileReader.prototype['readAs' + dataType]
+    var storeKey = camelcase(dataType)
+    var reDefaultEncoding = /utf-8/i
 
     return function readAs () {
-      var file = this.$file
-      return readAsPromise(
-        method,
-        file,
-        arguments
-      )
+      if (
+        dataType === 'Text' &&
+        arguments[0] &&
+        !reDefaultEncoding.test(arguments[0])
+      ) {
+        return readFile(method, this.$store.orignal, arguments)
+      }
+
+      if (!this.$store[storeKey]) {
+        this.$store[storeKey] = readFile(method, this.$store.orignal, arguments)
+      }
+
+      return this.$store[storeKey]
     }
   }
 
-  function getArrayBufferView (TypedArray) {
+  fileReaderSupportedDataTypes.forEach(function (dataType) {
+    PromisifyFile.prototype[camelcase(dataType)] = readFileAs(dataType)
+  })
+
+  function getArrayBufferView (viewType) {
+    var TypedArray = window[viewType]
+
     return function getArrayBufferView (byteOffset, length) {
       return this.arrayBuffer().then(function (buffer) {
         return new TypedArray(
@@ -103,15 +147,48 @@
       })
     }
   }
+  // get getArrayBufferView
+  arrayBufferViews.forEach(function (viewType) {
+    PromisifyFile.prototype[camelcase(viewType)] = getArrayBufferView(viewType)
+  })
 
-  function camelcase (s) {
-    return s[0].toLowerCase() + s.slice(1)
+  // get `Blob`
+  PromisifyFile.prototype.blob = function (options) {
+    var blob = new Blob(
+      [this.$store.orignal],
+      options || this.$store.orignal
+    )
+    return Promise.resolve(blob)
   }
 
-  function prepend (first, rest) {
-    return apply(concat, [first], rest)
+  // get `File`
+  PromisifyFile.prototype.file = function (name, options) {
+    var file = new File(
+      [this.$store.orignal],
+      name || this.$store.orignal.name,
+      options || this.$store.orignal
+    )
+    return Promise.resolve(file)
   }
 
+  // parse text content to `JSON`
+  function textToJSON (reviver) {
+    return function (text) {
+      return JSON.parse(text, reviver)
+    }
+  }
+
+  PromisifyFile.prototype.json = function (encoding, reviver) {
+    return this.text(encoding).then(textToJSON(reviver))
+  }
+
+  // get `URL`
+  PromisifyFile.prototype.url = function () {
+    var url = createObjectURL(this.$store.orignal)
+    return Promise.resolve(url)
+  }
+
+  // load as a `HTMLImageElement`
   function loadImage (src) {
     return new Promise(function (resolve, reject) {
       var image = new Image()
@@ -124,89 +201,44 @@
       image.src = src
     })
   }
-
-  function imageToImageBitmap (args) {
-    return function (image) {
-      return apply(createImageBitmap, root, prepend(image, args))
-    }
-  }
-
-  function textToJSON (reviver) {
-    return function (text) {
-      return JSON.parse(text, reviver)
-    }
-  }
-
-  function imageToImageData (sx, sy, sw, sh) {
-    var canvas = document.createElement('canvas')
-    var context = canvas.getContext('2d')
-
-    return function (image) {
-      var width = canvas.width = image.naturalWidth
-      var height = canvas.height = image.naturalHeight
-      context.drawImage(image, 0, 0, width, height)
-      sx = sx || 0
-      sy = sy || 0
-      sw = sw || width
-      sh = sh || height
-      return context.getImageData(sx, sy, sw, sh)
-    }
-  }
-
-  function PromisifyFile (blob) {
-    this.$file = blob
-  }
-
-  // use FileReader `readAs...` method
-  dataTypes.forEach(function (type) {
-    var readerMethod = 'readAs' + type
-    var fileMethod = camelcase(type)
-    PromisifyFile.prototype[fileMethod] = callFileReaderMethod(readerMethod)
-  })
-
-  // get getArrayBufferView
-  arrayBufferViews.forEach(function (type) {
-    var fileMethod = camelcase(type)
-    var TypedArray = window[type]
-    PromisifyFile.prototype[fileMethod] = getArrayBufferView(TypedArray)
-  })
-
-  // get `Blob`
-  PromisifyFile.prototype.blob = function (options) {
-    var blob = new Blob([this.$file], options || this.$file)
-    return Promise.resolve(blob)
-  }
-
-  // get `File`
-  PromisifyFile.prototype.file = function (name, options) {
-    var fileName = name || this.$file.name || 'blob'
-    var file = new File([this.$file], fileName, options || this.$file)
-    return Promise.resolve(file)
-  }
-
-  // parse text content to `JSON`
-  PromisifyFile.prototype.json = function (reviver) {
-    return this.text('UTF-8').then(textToJSON(reviver))
-  }
-
-  // get `URL`
-  PromisifyFile.prototype.url = function () {
-    return Promise.resolve(createObjectURL(this.$file))
-  }
-
-  // load as a `HTMLImageElement`
   PromisifyFile.prototype.image = function () {
     return this.dataURL().then(loadImage)
   }
 
   // get `ImageBitmap`
+  function imageToImageBitmap (args) {
+    return function (image) {
+      return apply(createImageBitmap, root, prepend(image, args))
+    }
+  }
   PromisifyFile.prototype.imageBitmap = function () {
     return this.image().then(imageToImageBitmap(arguments))
   }
 
   // get `ImageData`
+  var getImageData = (function () {
+    var canvas
+    var context
+
+    return function getImageData (sx, sy, sw, sh) {
+      sx = sx || 0
+      sy = sy || 0
+
+      return function getImageData (image) {
+        canvas = canvas || document.createElement('canvas')
+        context = context || canvas.getContext('2d')
+
+        var width = canvas.width = image.naturalWidth
+        var height = canvas.height = image.naturalHeight
+        sw = sw || width
+        sh = sh || height
+
+        return context.getImageData(sx, sy, sw, sh)
+      }
+    }
+  })()
   PromisifyFile.prototype.imageData = function (sx, sy, sw, sh) {
-    return this.image().then(imageToImageData(sx, sy, sw, sh))
+    return this.image().then(getImageData(sx, sy, sw, sh))
   }
 
   return umdExport('PromisifyFile', PromisifyFile)
