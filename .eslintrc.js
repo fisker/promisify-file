f50241f3fc06f5354d5e067c899b3d9ae9f94dc2
fisker <lionkay@gmail.com>
2019-02-26 23:50:51 +0800

    chore(linter): add linters

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
+/*!
+ * config file for eslint
+ * project https://github.com/xwtec/dotfiles
+ * primary link https://raw.githubusercontent.com/xwtec/dotfiles/master/eslint/.eslintrc.js
+ *
+ * configuration https://eslint.org/docs/user-guide/configuring
+ *
+ */
+
 module.exports = {
-  extends: ['@xwtec/eslint-config/legacy'],
+  root: true,
+  parserOptions: {},
+  extends: [
+    // @xwtec/eslint-config https://github.com/xwtec/eslint-config
+    '@xwtec/eslint-config/legacy',
+  ],
+  settings: {},
+  rules: {},
+  plugins: [],
+  overrides: [],
 }
