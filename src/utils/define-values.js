753aa6a2605a24b7ba670a55b0264bf4b81a1c82
fisker <lionkay@gmail.com>
2020-08-07 15:22:50 +0800

    style: fix code style

--- src/utils/define-values.js
+++ src/utils/define-values.js
@@ -1,21 +1,22 @@
 const {defineProperties} = Object
 
-function defineValues(
-  object,
-  values,
-  options = {
+function defineValues(object, values, options) {
+  const {configurable, writable, enumerable} = {
     configurable: true,
     writable: true,
     enumerable: true,
+    ...options,
   }
-) {
+
   const properties = {}
   const keys = Object.keys(values)
 
   for (const key of keys) {
     properties[key] = {
       value: values[key],
-      ...options,
+      configurable,
+      writable,
+      enumerable,
     }
   }
 
